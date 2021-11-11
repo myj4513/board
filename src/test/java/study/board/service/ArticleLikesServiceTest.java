2e89c27286d9c60d95ef2156f4ca449df3f4483e
myj4513 <yejungod@gmail.com>
2021-11-11 23:53:43 +0900

    practice test codes

--- src/test/java/study/board/service/ArticleLikesServiceTest.java
+++ src/test/java/study/board/service/ArticleLikesServiceTest.java
@@ -1,6 +1,7 @@
 package study.board.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -29,6 +30,7 @@ class ArticleLikesServiceTest {
     }
 
     @Test
+    @Disabled
     void countLikesTest(){
         int count = articleLikesService.countLikes(6);
         log.info("count : {}", count);
--- src/test/java/study/board/service/ArticleServiceTest.java
+++ src/test/java/study/board/service/ArticleServiceTest.java
@@ -2,6 +2,8 @@ package study.board.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +16,8 @@ public class ArticleServiceTest {
     private ArticleService articleService;
 
     @Test
+    @DisplayName("한글 입력가능 ❤💝💔❣💛")
+    @Disabled
     void getTotalPagesTest(){
         int totalPages = articleService.getTotalPages();
         Assertions.assertThat(totalPages).isEqualTo(6);
